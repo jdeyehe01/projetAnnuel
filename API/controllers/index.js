e757bc582bba632dbe749af83e7698248ca64de9
jdeyehe01 <34164147+jdeyehe01@users.noreply.github.com>
2018-04-22 23:59:03 +0200

    Add files via upload
    DEVAPI

---
+++ API/controllers/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  UserController: require('./user'),
+  ConferenceController: require('./conference'),
+  GuestController: require('./guest')
+}
---
+++ API/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const ModelIndex = require('./models');
+const RouteManager = require('./routes');
+
+ModelIndex
+.openDatabase()
+.then(_startServer)
+.catch((err) => {
+  console.error(err);
+});
+
+// INTERNAL
+
+function _startServer() {
+
+  const app = express();
+
+  RouteManager.attach(app);
+
+  app.listen(8080, function() {
+    console.log('Server started on 8080...');
+  });
+}
