21940adb2dcff6a6c52d8cc3d24da05bef97bb7e
Dopidot <dopidot@gmail.com>
2018-07-17 08:12:08 +0200

    Update user.js
    Remove unnecessary method decrypt

--- API/controllers/user.js
+++ API/controllers/user.js
@@ -63,11 +63,12 @@ UserController.encrypt = function(text){
   return crypted;
 }
 
+/*
 UserController.decrypt = function(text){
   var decipher = crypto.createDecipher(algorithm,password)
   var dec = decipher.update(text,'hex','utf8')
   dec += decipher.final('utf8');
   return dec;
-}
+}*/
 
 module.exports = UserController;
