32f87f3ad27fb14d6b4b38d38265a3e3dff58599
Sébastien Delbé <sebas9241@hotmail.fr>
2018-07-21 10:41:28 +0200

    commit du js et d'autres modif
    le js s'installe + réajustement du html

--- view/test.js
+++ view/test.js
@@ -1,4 +1,66 @@
 var request = new XMLHttpRequest();
+request.open('GET', 'http://sebastiendelbeportfolio.alwaysdata.net/conferences/getAll', true);
+
+request.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(movie => {
+      const card = document.createElement('div');
+      card.setAttribute('class', 'card');
+
+      const h1 = document.createElement('h1');
+      h1.textContent = movie.title;
+
+      const p = document.createElement('p');
+      movie.description = movie.description.substring(0, 300);
+      p.textContent = `${movie.description}...`;
+
+      container.appendChild(card);
+      card.appendChild(h1);
+      card.appendChild(p);
+    });
+  } else {
+    const errorMessage = document.createElement('marquee');
+    errorMessage.textContent = `Gah, it's not working!`;
+    app.appendChild(errorMessage);
+  }
+}
+
+request.send();
+
+
+
+
+
+function showListConf(jsonObj) {
+  var listconfs = jsonObj['conference'];
+      
+  for (var i = 0; i < listconfs.length; i++) {
+      var newDiv = document.createElement("div"); 
+      var myH2 = document.createElement('h2');
+      var myPara = document.createElement('p');
+
+      var input = document.createElement("input");
+        input.setAttribute("name","voirLaConf");
+        input.setAttribute("value","voir");
+        input.setAttribute("type","button");
+      input.setAttribute("onClick","self.location.href='listTaches.html'")
+
+      myH2.textContent = listconfs[i].name;
+      myPara.textContent = 'Elle dure : ' + listconfs[i].time + 'Description : ' + listconfs[i].description;
+
+      newDiv.appendChild(myH2);
+      newDiv.appendChild(myPara1);
+      newDiv.appendChild(input);
+
+      section.appendChild(myArticle);
+      alert("resultat good i think");
+  }
+}
+
+
+var request = new XMLHttpRequest();
 
 request.open('GET', "http://sebastiendelbeportfolio.alwaysdata.net/conferences/getAll", false);
 request.responseType = 'json';
@@ -13,26 +75,26 @@ function showListConf(jsonObj) {
   var listconfs = jsonObj['conference'];
       
   for (var i = 0; i < listconfs.length; i++) {
-  		var newDiv = document.createElement("div"); 
-	    var myH2 = document.createElement('h2');
-	    var myPara = document.createElement('p');
+      var newDiv = document.createElement("div"); 
+      var myH2 = document.createElement('h2');
+      var myPara = document.createElement('p');
 
-	    var input = document.createElement("input");
+      var input = document.createElement("input");
         input.setAttribute("name","voirLaConf");
         input.setAttribute("value","voir");
         input.setAttribute("type","button");
-    	input.setAttribute("onClick","self.location.href='listTaches.html'")
+      input.setAttribute("onClick","self.location.href='listTaches.html'")
 
-	    myH2.textContent = listconfs[i].name;
-	    myPara.textContent = 'Elle dure : ' + listconfs[i].time + 'Description : ' + listconfs[i].description;
+      myH2.textContent = listconfs[i].name;
+      myPara.textContent = 'Elle dure : ' + listconfs[i].time + 'Description : ' + listconfs[i].description;
 
-	    newDiv.appendChild(myH2);
-	    newDiv.appendChild(myPara1);
-	    newDiv.appendChild(input);
+      newDiv.appendChild(myH2);
+      newDiv.appendChild(myPara1);
+      newDiv.appendChild(input);
 
-	    section.appendChild(myArticle);
-	    alert("resultat good i think");
-	}
+      section.appendChild(myArticle);
+      alert("resultat good i think");
+  }
 }
 
 
