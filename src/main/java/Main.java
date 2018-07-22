24632e16b96f6d6e2111bdfdd019d23cf18265a2
Mickael Moreira <dopidot@gmail.com>
2018-07-22 17:16:26 +0200

    Merge conflict

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,50 +10,13 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-       // Parent root = FXMLLoader.load(getClass().getResource("View/loginView.fxml"));
-       //primaryStage.setTitle("Before Show - Authentification ");
+        Parent root = FXMLLoader.load(getClass().getResource("View/loginView.fxml"));
+       primaryStage.setTitle("Before Show - Authentification ");
 
-
+/*
         Parent root = FXMLLoader.load(getClass().getResource("View/beforShowWelcomeView.fxml"));
         primaryStage.setTitle("Before Show - Accueil ");
-
-
-        //URL url = getClass().getResource("View/CreateConfView.fxml");
-        //Parent root = FXMLLoader.load(url);
-       //primaryStage.setTitle("Before Show - Créer une conférence ");
-
-      //Parent root = FXMLLoader.load(getClass().getResource("View/createGuestView.fxml"));
-      //primaryStage.setTitle("Before Show - Ajouter un invité ");
-
-
-        //URL url = getClass().getResource("View/locateConfView.fxml");
-        //Parent root = FXMLLoader.load(url);
-       //primaryStage.setTitle("Before Show - Ajouter un lieu ");
-
-
-       // URL url = getClass().getResource("View/presentationConfView.fxml");
-       // Parent root = FXMLLoader.load(url);
-       //primaryStage.setTitle("Before Show - Ajouter une presentation ");
-
-     /* URL url = getClass().getResource("View/taskView.fxml");
-      Parent root = FXMLLoader.load(url);
-       primaryStage.setTitle("Before Show - Ajouter une tâche ");
-       */
-/*
-        URL url = getClass().getResource("View/budgetView.fxml");
-        Parent root = FXMLLoader.load(url);
-        primaryStage.setTitle("Before Show - Ajouter un budget ");
-
 */
-       // Parent root = FXMLLoader.load(getClass().getResource("View/loginView.fxml"));
-      // primaryStage.setTitle("Before Show - Login ");
-
-
-       /* URL url = getClass().getResource("View/UpdateConference.fxml");
-        Parent root = FXMLLoader.load(url);
-        primaryStage.setTitle("Before Show - Ajouter un budget ");
-        */
-
 
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root));
